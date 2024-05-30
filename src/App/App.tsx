24d4985067fb38d9894c6cf60006dd427da54d65
arash forghanbin <arashforghanbin@gmail.com>
2024-05-30 09:27:54 +0330

    fetch hourly data

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+
+const nLastElemetns = 10;
 
 function App() {
-  return <div className="App"></div>;
+  const [hourlyData, setHourlyData] = useState<any[] | undefined>(undefined);
+
+  const getHourlyVolume = async () => {
+    const res = await axios.get(
+      "https://min-api.cryptocompare.com/data/exchange/histohour?tsym=BTC&limit=10"
+    );
+    if (res.status === 200) {
+      const slicedData = res.data.Data.slice(-nLastElemetns);
+      console.log(slicedData);
+      setHourlyData(slicedData);
+    }
+  };
+
+  useEffect(() => {
+    getHourlyVolume();
+  }, []);
+
+  return (
+    <div className="App">
+      <ul>
+        {hourlyData &&
+          hourlyData?.map((item, index) => (
+            <li key={index}>
+              {new Date(item.time * 1000).toTimeString().slice(0, 5)}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
